05a427a65467a61f9cfe1dac572a6a88e662eff2
HiyoshiTurio <134475619+HiyoshiTurio@users.noreply.github.com>
2023-09-26 14:47:10 +0900

    [add] スコアスクリプトの改良

--- Assets/Scripts/Score/ScoreItem.cs
+++ Assets/Scripts/Score/ScoreItem.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 public class ScoreItem : MonoBehaviour
 {
     [SerializeField, TagName] string _playerTag;
-    [SerializeField] ScoreController scoreController;
+    ScoreController scoreController;
     //スコアコントローラーがついているオブジェクトをアタッチする
     [SerializeField] int _score = 0;
     //アイテムのスコアの値
+
+    void Start()
+    {
+        scoreController = FindObjectOfType<ScoreController>();
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == _playerTag)
         {
             scoreController.AddScore(_score);
+            Destroy(this.gameObject);
         } //プレイヤーがアイテムにぶつかったときScoreControllerのAddScoreメソッドが呼ばれる
     }
 }
