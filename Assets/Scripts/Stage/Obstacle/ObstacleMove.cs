6878b574eb93ff69af3f5d3051394afdf5c5b172
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-09-26 17:41:34 +0900

    [add]FieldGenerator作成

--- Assets/Scripts/Stage/Obstacle/ObstacleMove.cs
+++ Assets/Scripts/Stage/Obstacle/ObstacleMove.cs
@@ -1,4 +1,6 @@
+using System;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleMove : MonoBehaviour
@@ -6,10 +8,14 @@ public class ObstacleMove : MonoBehaviour
     [SerializeField] float _moveSpeed;
     [SerializeField] private float _endPos;
 
+    private void Start()
+    {
+        Move();
+    }
+
     public void Instantiate(float mag)
     {
         _moveSpeed *= mag;
-        Move();
     }
 
     public void Move()
