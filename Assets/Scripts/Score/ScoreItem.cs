3a8eec092944ebdba424761323b4b33218a2c44a
HiyoshiTurio <134475619+HiyoshiTurio@users.noreply.github.com>
2023-09-27 11:38:47 +0900

    [update] スコアにランダムで色が変化する機能追加

--- Assets/Scripts/Score/ScoreItem.cs
+++ Assets/Scripts/Score/ScoreItem.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 public class ScoreItem : MonoBehaviour
 {
     [SerializeField, TagName] string _playerTag;
-    ScoreController ScoreController;
-    //スコアコントローラーがついているオブジェクトをアタッチする
     [SerializeField] int _score = 0;
     //アイテムのスコアの値
-
+    ScoreController ScoreController;
     void Start()
     {
         ScoreController = FindObjectOfType<ScoreController>();
+        if (this.gameObject.tag == "FeverItem")
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+            return;
+        }
+        int _randam = Random.Range(0, 3);
+        if (_randam == 0) GetComponent<Renderer>().material.color = Color.red;
+        else if(_randam == 1) GetComponent<Renderer>().material.color = Color.blue;
+        else GetComponent<Renderer>().material.color = Color.yellow;
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -23,6 +30,6 @@ public class ScoreItem : MonoBehaviour
             Destroy(this.gameObject);
             if (this.gameObject.tag == "FeverItem") return;
             ScoreController.ItemCount();
-        } //プレイヤーがアイテムにぶつかったときScoreControllerのAddScoreメソッドが呼ばれる
+        } //プレイヤーがアイテムにぶつかったときScoreControllerのAddScore ItemCountメソッドが呼ばれる
     }
 }
